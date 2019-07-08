1b43892f74c6f517da3ab004f7fdebd4e11d7879
Eunsung Lee <okskmk2@naver.com>
2019-07-08 20:20:46 +0900

    b2

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,17 +1,109 @@
 <i18n>
   en: 
-    title: Contact
+    title: Request
+    q1: Which platform do you want?
+    q2: What is the purpose?
+    web: Web
+    android_mobile: Android
+    linux: Linux
+    Next: Next
   ko: 
     title: 의뢰하기
-  ja: 
-    title: こんにちは、世界！
-  zh: 
-    title: 你好、世界！
+    q1: 어떤 플랫폼을 원하세요?
+    q2: 용도가 뭔가요?
+    web: 웹
+    android_mobile: 안드로이드
+    linux: 리눅스
+    Next: 다음
 </i18n>
 
 <template>
   <div>
     <h1>{{ $t('title') }}</h1>
+    <section>
+      <div class="stepper">
+        <span class="tag">1</span>
+        <span class="tag">2</span>
+        <span class="tag">3</span>
+      </div>
+      <div class="question">{{ $t('q1') }}</div>
+      <div class="answer">
+        <label for="web">
+          {{ $t('web') }}
+          <input type="checkbox" id="web" />
+        </label>
+        <br />
+        <label for="android_mobile">
+          {{ $t('android_mobile') }}
+          <input type="checkbox" id="android_mobile" />
+        </label>
+        <br />
+        <label for="linux">
+          {{ $t('linux') }}
+          <input type="checkbox" id="linux" />
+        </label>
+      </div>
+      <div class="question">{{ $t('q2') }}</div>
+      <div class="answer">
+        <input type="text" maxlength="300" class="a2" />
+      </div>
+      <div class="row jc-end">
+        <button>{{ $t('Next') }}</button>
+      </div>
+    </section>
   </div>
 </template>
+<style scoped>
+section {
+  padding: 3rem;
+  border-radius: 5px;
+  background-color: #fefeff;
+  box-shadow: 1px 1px 2px 1px #bbb;
+  margin-top: 2rem;
+}
+.question {
+  font-size: 1.5rem;
+}
+.answer {
+  margin: 1rem 1rem 2rem;
+  line-height: 2;
+}
+.a2 {
+  margin-top: 2rem;
+  display: block;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 1rem;
+}
+
+button {
+  font-size: 20px;
+  padding: 4px 12px;
+  color: #333;
+}
+
+.stepper{
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 2rem;
+}
+
+.tag {
+  width: 40px;
+  height: 40px;
+  border-radius: 40px;
+  background-color: #0089f4;
+  color: white;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+}
+
+@media screen and (max-width: 650px) {
+  section {
+    padding: 1rem;
+  }
+}
+</style>
 
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -12,60 +12,67 @@
 <template>
   <div>
     <h1>{{ $t('title') }}</h1>
-    <section>
-      <article>
-        <h2>fbank</h2>
-        <p>은행, IFRS9</p>
-        <p>jsp</p>
-      </article>
-      <article>
-        <h2>건물 관리자</h2>
-        <p>건물관리 웹사이트</p>
-        <p>vue, django</p>
-      </article>
-      <article>
-        <h2>민원24</h2>
-        <p>민원청구</p>
-        <p>전사정부프레임워크</p>
-      </article>
-      <article>
-        <h2>부하분산시설</h2>
-        <p>대규모 작업을 처리하기 위한 부하분산 시스템</p>
-        <p>jsp</p>
-      </article>
-      <article>
-        <h2>식당</h2>
-        <p>각종 메뉴 소개, 찾아오늘 길</p>
-        <p>윅스</p>
-      </article>
-      <article>
-        <h2>마켓</h2>
-        <p>누구나 상품을 올릴 수 있다.</p>
-        <p>고도몰</p>
-      </article>
-      <article>
-        <h2>사회문제연구소</h2>
-        <p>정책제안</p>
-        <p>vue, express</p>
-      </article>
-      <article>
-        <h2>PMS</h2>
-        <p>프로젝트 관리</p>
-        <p>django</p>
-      </article>
-    </section>
+    <article>
+      <h2>fbank</h2>
+      <p>은행, IFRS9</p>
+      <p>jsp</p>
+    </article>
+    <article>
+      <h2>건물 관리자</h2>
+      <p>건물관리 웹사이트</p>
+      <p>vue, django</p>
+    </article>
+    <article>
+      <h2>민원24</h2>
+      <p>민원청구</p>
+      <p>전사정부프레임워크</p>
+    </article>
+    <article>
+      <h2>부하분산시설</h2>
+      <p>대규모 작업을 처리하기 위한 부하분산 시스템</p>
+      <p>jsp</p>
+    </article>
+    <article>
+      <h2>식당</h2>
+      <p>각종 메뉴 소개, 찾아오늘 길</p>
+      <p>윅스</p>
+    </article>
+    <article>
+      <h2>마켓</h2>
+      <p>누구나 상품을 올릴 수 있다.</p>
+      <p>고도몰</p>
+    </article>
+    <article>
+      <h2>사회문제연구소</h2>
+      <p>정책제안</p>
+      <p>vue, express</p>
+    </article>
+    <article>
+      <h2>PMS</h2>
+      <p>프로젝트 관리</p>
+      <p>django</p>
+    </article>
   </div>
 </template>
 <style scoped>
-h2{
+h2 {
   margin: 1rem;
 }
 article {
-    display: inline-block;
-    width: 574px;
-    border: 1px solid #ddd;
-    height: 20rem;
-    margin: 0 1rem 1rem 0;
+  padding: 2rem;
+  display: inline-block;
+  width: 510px;
+  border: 1px solid #ddd;
+  height: 20rem;
+  margin: 0 1rem 1rem 0;
+}
+@media screen and (max-width: 650px) {
+  article {
+    padding: 1rem;
+    width: auto;
+    display: block;
+    margin: 1rem 0;
+  }
 }
 </style>
 
