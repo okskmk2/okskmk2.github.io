838f67e2f003c03a8c7866bd2a7969672b0bf234
okskmk2 <okskmk2@amoeba.co.kr>
2022-01-25 22:27:10 +0900

    이력 갱신

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -40,16 +40,16 @@
     <section>
       <h2>
         <i class="fa fa-address-card fa-fw mr5"></i>
-        {{ $t('basic')}}
+        {{ $t("basic") }}
       </h2>
       <article>
         <p>
           <i class="fa fa-briefcase fa-fw mr5"></i>
-          {{ $t('webdeveloper')}}
+          {{ $t("webdeveloper") }}
         </p>
         <p>
           <i class="fa fa-home fa-fw mr5"></i>
-          {{ $t('address')}}
+          {{ $t("address") }}
         </p>
         <p>
           <i class="fa fa-envelope fa-fw mr5"></i>
@@ -57,106 +57,115 @@
         </p>
         <p>
           <i class="fa fa-phone fa-fw mr5"></i>
-          {{ $t('phonenumber')}}
+          {{ $t("phonenumber") }}
         </p>
         <p>
           <i class="fa fa-comment fa-fw mr5"></i>
-          {{ $t('moto')}}
+          {{ $t("moto") }}
         </p>
       </article>
     </section>
     <section>
       <h2>
         <i class="fa fa-suitcase fa-fw mr5"></i>
-        {{ $t('workExperience')}}
+        {{ $t("workExperience") }}
       </h2>
       <article>
         <h3>SK이노베이션 위더스 프로젝트</h3>
-        <h4>
-          <i class="fa fa-calendar fa-fw mr5"></i>2021-2 ~
-        </h4>
+        <h4><i class="fa fa-calendar fa-fw mr5"></i>2021-2 ~</h4>
         <p>물성예측을 위한 인공지능 기계학습 플랫폼을 운영하고 있습니다.</p>
-        <p>기술스택 : vuejs, python, pandas, mariaDB, redis, azure<p>
+        <p>기술스택 : vuejs, python, pandas, mariaDB, redis, azure</p>
+        <p></p>
         <p>역할 : 운영</p>
       </article>
       <article>
+        <h3>LG전자 냉장고 매장앱 접근성 보안 퍼블리싱</h3>
+        <h4><i class="fa fa-calendar fa-fw mr5"></i>2021-11 ~ 2021-12</h4>
+        <p>키보드 조작 지원, 시맨틱 태그로 교체작업, 사용성 강화를 위한 UI 재정비 작업</p>
+        <p>기술스택 : vuejs, sass</p>
+        <p></p>
+        <p>역할 : 퍼블리싱</p>
+      </article>
+      <article>
         <h3>LG전자 담다 프로젝트</h3>
-        <h4>
-          <i class="fa fa-calendar fa-fw mr5"></i>2021-9 ~ 2021-10
-        </h4>
+        <h4><i class="fa fa-calendar fa-fw mr5"></i>2021-9 ~ 2021-10</h4>
         <p>전자제품 API 도큐먼트 퍼블리싱 작업을 하였습니다.</p>
-        <p>기술스택 : vuejs<p>
+        <p>기술스택 : vuejs</p>
+        <p></p>
         <p>역할 : 프론트앤드</p>
       </article>
       <article>
         <h3>LG전자 퀵헬프 웹프로젝트</h3>
-        <h4>
-          <i class="fa fa-calendar fa-fw mr5"></i>2020-12 ~ 2021-2
-        </h4>
+        <h4><i class="fa fa-calendar fa-fw mr5"></i>2020-12 ~ 2021-2</h4>
         <p>LG전자 퀵헬프에서 인증 모듈을 구현하였습니다.</p>
-        <p>기술스택 : java, spring security, jwt, redis, oracle<p>
+        <p>기술스택 : java, spring security, jwt, redis, oracle</p>
+        <p></p>
         <p>역할 : 개발</p>
       </article>
       <article>
         <h3>삼성 녹스팀즈 웹클라이언트 운영개발</h3>
-        <h4>
-          <i class="fa fa-calendar fa-fw mr5"></i>2020-06 ~ 2020-12
-        </h4>
+        <h4><i class="fa fa-calendar fa-fw mr5"></i>2020-06 ~ 2020-12</h4>
         <p>삼성SDS에서 그룹웨어 웹 클라이언트를 개발 운영하였습니다.</p>
-        <p>기술스택 : React, Websocket<p>
+        <p>기술스택 : React, Websocket</p>
+        <p></p>
         <p>역할 : 개발팀장</p>
-      </article>     
+      </article>
       <article>
         <h3>바은 자기개발SNS 프로젝트</h3>
-        <h4>
-          <i class="fa fa-calendar fa-fw mr5"></i>2019-07 ~ 2020-06
-        </h4>
+        <h4><i class="fa fa-calendar fa-fw mr5"></i>2019-07 ~ 2020-06</h4>
         <p>글로벌 공동목표 관리 플랫폼 웹서비스를 개발하였습니다.</p>
         <p>기술스택 : spring boot, jsp, mariaDB</p>
         <p>역할 : 개발팀장</p>
-      </article>       
+      </article>
       <article>
         <h3>SK하이닉스 인프라 유지보수</h3>
-        <h4>
-          <i class="fa fa-calendar fa-fw mr5"></i>2018-01 ~ 2019-07
-        </h4>
-        <p>IBM <span title="대규모 부하분산 시스템">LSF</span> 시스템을 파이썬으로 웹, API서버로 확장하였습니다. 개발한 서비스를 국내 2대 서버, 해외 4대 서버를 운영하였습니다.</p>
+        <h4><i class="fa fa-calendar fa-fw mr5"></i>2018-01 ~ 2019-07</h4>
+        <p>
+          IBM <span title="대규모 부하분산 시스템">LSF</span> 시스템을
+          파이썬으로 웹, API서버로 확장하였습니다. 개발한 서비스를 국내 2대
+          서버, 해외 4대 서버를 운영하였습니다.
+        </p>
         <p>기술스택 : Oracle SQL, Mysql, LSF python API, django, Java</p>
         <p>역할 : 설계, 개발, 운영</p>
       </article>
       <article>
         <h3>한화생명 온슈어 웹프로젝트</h3>
-        <h4>
-          <i class="fa fa-calendar fa-fw mr5"></i>2017-09 ~ 2017-11
-        </h4>
-        <p>자바 스프링과 JSP를 사용하여 배치시스템을 개발하고, 해피콜 개발, 카카오링크 연동을 하였습니다. SQL Server를 사용하였습니다.</p>
+        <h4><i class="fa fa-calendar fa-fw mr5"></i>2017-09 ~ 2017-11</h4>
+        <p>
+          자바 스프링과 JSP를 사용하여 배치시스템을 개발하고, 해피콜 개발,
+          카카오링크 연동을 하였습니다. SQL Server를 사용하였습니다.
+        </p>
         <p>기술스택 : SQL Server, Java, Spring MVC</p>
         <p>역할 : 개발</p>
       </article>
       <article>
         <h3>아트투게더</h3>
-        <h4>
-          <i class="fa fa-calendar fa-fw mr5"></i>2017-07 ~ 2017-09
-        </h4>
-        <p>스타트업 초기 기술 로드맵과 자바 스프링을 사용하여 프로토타입의 웹 사이트를 개발하였습니다. MariaDB를 사용하였습니다. 자료 수집을 위해 웹크롤링 작업도 병행하였습니다.</p>
+        <h4><i class="fa fa-calendar fa-fw mr5"></i>2017-07 ~ 2017-09</h4>
+        <p>
+          스타트업 초기 기술 로드맵과 자바 스프링을 사용하여 프로토타입의 웹
+          사이트를 개발하였습니다. MariaDB를 사용하였습니다. 자료 수집을 위해
+          웹크롤링 작업도 병행하였습니다.
+        </p>
         <p>기술스택 : MariaDB, Java, Spring MVC</p>
         <p>역할 : 설계, 개발, 운영</p>
       </article>
       <article>
         <h3>농협 IFRS9 구축</h3>
-        <h4>
-          <i class="fa fa-calendar fa-fw mr5"></i>2016-09 ~ 2017-05
-        </h4>
-        <p>농협 자체 화면 개발도구로 화면을 개발하였고 서버 환경에서 C언어로 회계업무로직을 개발하였습니다. OracleSQL를 사용하였습니다.</p>
+        <h4><i class="fa fa-calendar fa-fw mr5"></i>2016-09 ~ 2017-05</h4>
+        <p>
+          농협 자체 화면 개발도구로 화면을 개발하였고 서버 환경에서 C언어로
+          회계업무로직을 개발하였습니다. OracleSQL를 사용하였습니다.
+        </p>
         <p>기술스택 : OracleSQL, Jscript(이지빌더), Proframe C</p>
         <p>역할 : 개발</p>
       </article>
       <article>
         <h3>농협 핀테크 오픈 API 플랫폼 고도화</h3>
-        <h4>
-          <i class="fa fa-calendar fa-fw mr5"></i>2016-04 ~ 2016-09
-        </h4>
-        <p>자바를 이용하여 핀테크 오픈 API를 개발하였습니다. 신용카드결재API, 제휴기관API를 개발하였습니다.</p>
+        <h4><i class="fa fa-calendar fa-fw mr5"></i>2016-04 ~ 2016-09</h4>
+        <p>
+          자바를 이용하여 핀테크 오픈 API를 개발하였습니다. 신용카드결재API,
+          제휴기관API를 개발하였습니다.
+        </p>
         <p>기술스택 : Java / JSP</p>
         <p>역할 : 개발</p>
       </article>
@@ -214,8 +223,8 @@
 </template>
 <script>
 export default {
-  name:'Profile'
-}
+  name: "Profile",
+};
 </script>
 
 <style scoped>
@@ -231,7 +240,7 @@ h4 {
   font-weight: normal;
   color: #555;
 }
-p{
+p {
   line-height: 1.5;
 }
 section {
@@ -241,7 +250,7 @@ section {
   box-shadow: 1px 1px 2px 1px #bbb;
   margin-top: 1rem;
 }
-article:not(:last-child){
+article:not(:last-child) {
   border-bottom: 1px solid #ddd;
 }
 </style>
