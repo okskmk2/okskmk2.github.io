529b7d8ca94aa21194a262cef2fa4d9a7ba31df9
eunsung lee <okskmk2@gmail.com>
2025-03-18 13:56:28 +0900

    경력 업데이트

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,43 +1,45 @@
 <template>
   <div>
     <section>
-      <h2>
-        <i class="fa fa-address-card fa-fw mr5"></i> 기본정보
-      </h2>
+      <h2><i class="fa fa-address-card fa-fw mr5"></i> 기본정보</h2>
       <article>
+        <p><i class="fa fa-briefcase fa-fw mr5"></i> 웹 개발자</p>
+        <p><i class="fa fa-home fa-fw mr5"></i> 한국 부천시</p>
+        <p><i class="fa fa-envelope fa-fw mr5"></i> eunsung.lee@baeun.com</p>
+        <p><i class="fa fa-phone fa-fw mr5"></i> 010-4920-0523</p>
         <p>
-          <i class="fa fa-briefcase fa-fw mr5"></i> 웹 개발자
-        </p>
-        <p>
-          <i class="fa fa-home fa-fw mr5"></i> 한국 부천시
-        </p>
-        <p>
-          <i class="fa fa-envelope fa-fw mr5"></i> eunsung.lee@baeun.com
-        </p>
-        <p>
-          <i class="fa fa-phone fa-fw mr5"></i> 010-4920-0523
-        </p>
-        <p>
-          <i class="fa fa-comment fa-fw mr5"></i> 읽기 쉬운 사이트가 가장 좋은 사이트이다.
+          <i class="fa fa-comment fa-fw mr5"></i> 읽기 쉬운 사이트가 가장 좋은
+          사이트이다.
         </p>
       </article>
     </section>
     <section>
-      <h2>
-        <i class="fa fa-suitcase fa-fw mr5"></i> 실무경험
-      </h2>
+      <h2><i class="fa fa-suitcase fa-fw mr5"></i> 실무경험</h2>
+      <article>
+        <h3>SK텔레콤 MAMF 프로젝트</h3>
+        <h4><i class="fa fa-calendar fa-fw mr5"></i>2024-11 ~</h4>
+        <p>AI 기술을 활용한 마케터를 위한 플랫폼을 운영하고 있습니다.</p>
+        <p>기술스택 : nextjs</p>
+        <p></p>
+        <p>역할 : 운영</p>
+      </article>
       <article>
         <h3>SK이노베이션 위더스 프로젝트</h3>
-        <h4><i class="fa fa-calendar fa-fw mr5"></i>2021-2 ~</h4>
-        <p>물성예측을 위한 인공지능 기계학습 플랫폼을 운영하고 있습니다.</p>
-        <p>기술스택 : vuejs, python, pandas, mariaDB, redis, azure</p>
+        <h4><i class="fa fa-calendar fa-fw mr5"></i>2021-2 ~ 2024-2</h4>
+        <p>물성예측을 위한 인공지능 기계학습 플랫폼을 운영하였습니다.</p>
+        <p>
+          기술스택 : vuejs, python, pandas, mariaDB, redis, azure, kubernates
+        </p>
         <p></p>
         <p>역할 : 운영</p>
       </article>
       <article>
         <h3>LG전자 냉장고 매장앱 접근성 보안 퍼블리싱</h3>
         <h4><i class="fa fa-calendar fa-fw mr5"></i>2021-11 ~ 2021-12</h4>
-        <p>키보드 조작 지원, 시맨틱 태그로 교체작업, 사용성 강화를 위한 UI 재정비 작업</p>
+        <p>
+          키보드 조작 지원, 시맨틱 태그로 교체작업, 사용성 강화를 위한 UI 재정비
+          작업
+        </p>
         <p>기술스택 : vuejs, sass</p>
         <p></p>
         <p>역할 : 퍼블리싱</p>
@@ -69,7 +71,9 @@
       <article>
         <h3>고려대학교 인공지능 연구소 프로젝트</h3>
         <h4><i class="fa fa-calendar fa-fw mr5"></i>2019-11 ~ 2019-12</h4>
-        <p>인공지능 컴퓨터 비전을 통해 휴머노이드 로봇 개발에 참여하였습니다.</p>
+        <p>
+          인공지능 컴퓨터 비전을 통해 휴머노이드 로봇 개발에 참여하였습니다.
+        </p>
         <p>기술스택 : python</p>
         <p>역할 : 개발</p>
       </article>
@@ -83,7 +87,10 @@
       <article>
         <h3>SK하이닉스 인프라 유지보수</h3>
         <h4><i class="fa fa-calendar fa-fw mr5"></i>2018-01 ~ 2019-07</h4>
-        <p> IBM <span title="대규모 부하분산 시스템">LSF</span> 시스템을 파이썬으로 웹, API서버로 확장하였습니다. 개발한 서비스를 국내 2대 서버, 해외 4대 서버를 운영하였습니다.
+        <p>
+          IBM <span title="대규모 부하분산 시스템">LSF</span> 시스템을
+          파이썬으로 웹, API서버로 확장하였습니다. 개발한 서비스를 국내 2대
+          서버, 해외 4대 서버를 운영하였습니다.
         </p>
         <p>기술스택 : Oracle SQL, Mysql, LSF python API, django, Java</p>
         <p>역할 : 설계, 개발, 운영</p>
@@ -91,28 +98,41 @@
       <article>
         <h3>한화생명 온슈어 웹프로젝트</h3>
         <h4><i class="fa fa-calendar fa-fw mr5"></i>2017-09 ~ 2017-11</h4>
-        <p> 자바 스프링과 JSP를 사용하여 배치시스템을 개발하고, 해피콜 개발, 카카오링크 연동을 하였습니다. SQL Server를 사용하였습니다. </p>
+        <p>
+          자바 스프링과 JSP를 사용하여 배치시스템을 개발하고, 해피콜 개발,
+          카카오링크 연동을 하였습니다. SQL Server를 사용하였습니다.
+        </p>
         <p>기술스택 : SQL Server, Java, Spring MVC</p>
         <p>역할 : 개발</p>
       </article>
       <article>
         <h3>아트투게더</h3>
         <h4><i class="fa fa-calendar fa-fw mr5"></i>2017-07 ~ 2017-09</h4>
-        <p> 스타트업 초기 기술 로드맵과 자바 스프링을 사용하여 프로토타입의 웹 사이트를 개발하였습니다. MariaDB를 사용하였습니다. 자료 수집을 위해 웹크롤링 작업도 병행하였습니다. </p>
+        <p>
+          스타트업 초기 기술 로드맵과 자바 스프링을 사용하여 프로토타입의 웹
+          사이트를 개발하였습니다. MariaDB를 사용하였습니다. 자료 수집을 위해
+          웹크롤링 작업도 병행하였습니다.
+        </p>
         <p>기술스택 : MariaDB, Java, Spring MVC</p>
         <p>역할 : 설계, 개발, 운영</p>
       </article>
       <article>
         <h3>농협 IFRS9 구축</h3>
         <h4><i class="fa fa-calendar fa-fw mr5"></i>2016-09 ~ 2017-05</h4>
-        <p> 농협 자체 화면 개발도구로 화면을 개발하였고 서버 환경에서 C언어로 회계업무로직을 개발하였습니다. OracleSQL를 사용하였습니다. </p>
+        <p>
+          농협 자체 화면 개발도구로 화면을 개발하였고 서버 환경에서 C언어로
+          회계업무로직을 개발하였습니다. OracleSQL를 사용하였습니다.
+        </p>
         <p>기술스택 : OracleSQL, Jscript(이지빌더), Proframe C</p>
         <p>역할 : 개발</p>
       </article>
       <article>
         <h3>농협 핀테크 오픈 API 플랫폼 고도화</h3>
         <h4><i class="fa fa-calendar fa-fw mr5"></i>2016-04 ~ 2016-09</h4>
-        <p> 자바를 이용하여 핀테크 오픈 API를 개발하였습니다. 신용카드결재API, 제휴기관API를 개발하였습니다. </p>
+        <p>
+          자바를 이용하여 핀테크 오픈 API를 개발하였습니다. 신용카드결재API,
+          제휴기관API를 개발하였습니다.
+        </p>
         <p>기술스택 : Java / JSP</p>
         <p>역할 : 개발</p>
       </article>
@@ -124,51 +144,40 @@
       </h2>
       <article>
         <h3>디자인툴 피그마 과정</h3>
-        <h4>
-          <i class="fa fa-calendar fa-fw mr5"></i>2023-02 ~ 2023-04
-        </h4>
+        <h4><i class="fa fa-calendar fa-fw mr5"></i>2023-02 ~ 2023-04</h4>
         <p>웹 디자인, 앱 디자인 툴인 피그마를 활용하여 디자인을 배우는 과정</p>
       </article>
       <article>
         <h3>웹기획 고급과정</h3>
-        <h4>
-          <i class="fa fa-calendar fa-fw mr5"></i>2018-12
-        </h4>
-        <p>기획, 사용자조사, UI/UX기획, 제안서, 견적서 작성법, 프로젝트 관리 실무과정</p>
+        <h4><i class="fa fa-calendar fa-fw mr5"></i>2018-12</h4>
+        <p>
+          기획, 사용자조사, UI/UX기획, 제안서, 견적서 작성법, 프로젝트 관리
+          실무과정
+        </p>
       </article>
       <article>
         <h3>Angular 5 개발과정</h3>
-        <h4>
-          <i class="fa fa-calendar fa-fw mr5"></i>2018-04
-        </h4>
+        <h4><i class="fa fa-calendar fa-fw mr5"></i>2018-04</h4>
         <p>Angular 5 프론트엔드개발 32시간 수료</p>
       </article>
       <article>
         <h3>한국인터넷 진흥원 핀테크 서비스개발</h3>
-        <h4>
-          <i class="fa fa-calendar fa-fw mr5"></i>2017-07
-        </h4>
+        <h4><i class="fa fa-calendar fa-fw mr5"></i>2017-07</h4>
         <p>핀테크 웹서비스 개발 사례연구 및 프로젝트 리딩</p>
       </article>
       <article>
         <h3>방송통신대학교 컴퓨터과학과</h3>
-        <h4>
-          <i class="fa fa-calendar fa-fw mr5"></i>2014 ~ 2018
-        </h4>
+        <h4><i class="fa fa-calendar fa-fw mr5"></i>2014 ~ 2018</h4>
         <p>주요 이수과목 : 소프트웨어 공학, 데이터베이스 설계</p>
       </article>
       <article>
         <h3>정보처리산업기사</h3>
-        <h4>
-          <i class="fa fa-calendar fa-fw mr5"></i>2016-04
-        </h4>
+        <h4><i class="fa fa-calendar fa-fw mr5"></i>2016-04</h4>
         <p>주요 내용 : 알고리즘, 데이터베이스 구조, 네트워크 보안</p>
       </article>
       <article>
         <h3>중앙처리컴퓨터학원</h3>
-        <h4>
-          <i class="fa fa-calendar fa-fw mr5"></i>2015-10 ~ 2016-04
-        </h4>
+        <h4><i class="fa fa-calendar fa-fw mr5"></i>2015-10 ~ 2016-04</h4>
         <p>수석 수료</p>
         <p>주요 이수기술 : OracleSQL, Java, Javascript, Android</p>
       </article>
@@ -205,7 +214,7 @@ section {
   padding: 1rem;
   border-radius: 5px;
   background-color: #fefeff;
-  box-shadow: 1px 1px 2px 1px #bbb;
+  box-shadow: 1px 1px 4px 1px #ddd;
   margin-top: 1rem;
 }
 
